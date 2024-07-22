7f3be891247d11ecc551f312456cd8b9e6f7bd1d
amachlou <abderrahim.machlou@gmail.com>
2024-07-22 11:44:18 +0100

    added spring data rest

---
+++ src/main/java/com/adminproject/admin/DAO/CourseRepositoryV2.java
@@ -0,0 +1,14 @@
+package com.adminproject.admin.DAO;
+
+import com.adminproject.admin.Entities.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CourseRepositoryV2 extends JpaRepository<Course, Long> {
+
+}
